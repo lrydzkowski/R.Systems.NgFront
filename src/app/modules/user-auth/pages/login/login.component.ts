1a4b322b2c843027f4dac0c32921ec62ee6452df
Łukasz Rydzkowski <lukasz_rydzkowski@loco.pl>
2021-06-16 21:54:49 +0200

    Start work on login form

--- src/app/modules/user-auth/pages/login/login.component.ts
+++ src/app/modules/user-auth/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormHandlerService } from 'src/app/shared/services/form-handler.service';
 
 @Component({
   selector: 'user-auth-login',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup = this.formBuilder.group({
+    login: ['', [
+      Validators.required
+    ]],
+    password: ['', [
+      Validators.required
+    ]]
+  });
+
+  isProcessing: boolean = false;
+
+  constructor(
+    public formHandler: FormHandlerService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.formHandler.setFormGroup(this.form);
+  }
+
+  submit(): void {
+    console.log('submit');
+    if (this.isProcessing) {
+      return;
+    }
+    if (!this.formHandler.isFormValid()) {
+      return;
+    }
+    this.isProcessing = true;
   }
 
 }
--- src/app/modules/user-auth/user-auth.module.ts
+++ src/app/modules/user-auth/user-auth.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CardModule } from 'primeng/card';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { FocusTrapModule } from 'primeng/focustrap';
 
 import { LoginComponent } from './pages/login/login.component';
 import { UserAuthRoutingModule } from './user-auth-routing.module';
@@ -17,11 +20,14 @@ import { UserAuthRoutingModule } from './user-auth-routing.module';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
 
     PasswordModule,
     CardModule,
     ButtonModule,
     InputTextModule,
+    ProgressSpinnerModule,
+    FocusTrapModule,
 
     UserAuthRoutingModule
   ]
