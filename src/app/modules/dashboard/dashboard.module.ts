d1ee000cf1fc7c6ddfc9849ce1ac90c515d0d72a
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2021-06-22 19:25:12 +0200

    Prepare initial version of dashboard

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CardModule } from 'primeng/card';
+
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { TileComponent } from './components/tile/tile.component';
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    TileComponent
   ],
   imports: [
     CommonModule,
+
+    CardModule,
+
+    SharedModule,
     DashboardRoutingModule
   ]
 })
