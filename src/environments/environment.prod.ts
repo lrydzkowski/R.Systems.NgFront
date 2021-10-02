5c457a80e77e43bf4a4c5646a441701ab9896c18
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2021-10-02 20:31:03 +0200

    docs(readme): correct grammar

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import npm from '../../package.json';
 export const environment = {
   production: true,
   version: npm.version,
-  build: '2021-09-27T16:34:17'
+  build: '2021-10-02T20:31:19'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import npm from '../../package.json';
 export const environment = {
   production: false,
   version: npm.version,
-  build: '2021-09-27T16:34:17'
+  build: '2021-10-02T20:31:19'
 };
 
 /*
