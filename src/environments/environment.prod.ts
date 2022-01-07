38a2d155f166ba7ab373f90d0bb38ff5031b6659
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2022-01-07 17:28:48 +0100

    build: improve esling configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import npm from '../../package.json';
 export const environment = {
   production: true,
   version: npm.version,
-  build: '2022-01-06T19:43:03'
+  build: '2022-01-07T17:28:57'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import npm from '../../package.json';
 export const environment = {
   production: false,
   version: npm.version,
-  build: '2022-01-06T19:43:03'
+  build: '2022-01-07T17:28:57'
 };
 
 /*
