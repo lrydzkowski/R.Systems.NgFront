8c90b6f201e38e254b8aac129c3a4417a15d9ae3
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2022-01-09 17:36:18 +0100

    build: increase version number

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import npm from '../../package.json';
 export const environment = {
   production: true,
   version: npm.version,
-  build: '2022-01-09T17:35:30'
+  build: '2022-01-09T17:36:28'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import npm from '../../package.json';
 export const environment = {
   production: false,
   version: npm.version,
-  build: '2022-01-09T17:35:30'
+  build: '2022-01-09T17:36:28'
 };
 
 /*
